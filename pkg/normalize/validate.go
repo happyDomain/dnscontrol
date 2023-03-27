9698b4a2ae788596cc93421b9ee39e8a9e0a29e7
Paul <itsascambutmailmeanyway@gmail.com>
2023-03-27 15:32:51 +0200

    OPENPGPKEY record type
    Step 1: Update RecordConfig in models/record.go ✅
    
    Step 2: Add a capability for the record ✅
    +providerCapabilityChecks ✅
    +TestCapabilitiesAreFiltered ✅
    
    Step 3: Add a helper function ✅
    
    OPENPGPKEY()
    
    Plus extra internal functions to hash and process
    
    Step 4: Search for #rtype_variations ✅
    
    Step 5: Add a parse_tests test case ✅
    
    Step 6: Add an integrationTest test case ✅
    
    --- PASS: TestDNSProviders (0.01s)
        --- PASS: TestDNSProviders/example.com (0.01s)
            --- PASS: TestDNSProviders/example.com/Clean_Slate:Empty (0.00s)
            --- PASS: TestDNSProviders/example.com/28:OPENPGPKEY:OPENPGPKEY_create_first_record_->_36_bytes (0.00s)
            --- PASS: TestDNSProviders/example.com/28:OPENPGPKEY:OPENPGPKEY_create_second_record_->_304_bytes (0.00s)
            --- PASS: TestDNSProviders/example.com/28:OPENPGPKEY:OPENPGPKEY_create_second_subrecord_->_3004_bytes (0.00s)
            --- PASS: TestDNSProviders/example.com/28:OPENPGPKEY:OPENPGPKEY_delete_record_->_3004_bytes (0.00s)
            --- PASS: TestDNSProviders/example.com/28:OPENPGPKEY:OPENPGPKEY_change_target_->_304_to_3004_bytes (0.00s)
            --- PASS: TestDNSProviders/example.com/Post_cleanup:Empty (0.00s)
    Step 7: Support more providers ✅ (bind, axfr)
    
    Step 8: Write documentation ✅

--- pkg/normalize/validate.go
+++ pkg/normalize/validate.go
@@ -68,6 +68,7 @@ func validateRecordTypes(rec *models.RecordConfig, domain string, pTypes []strin
 		"MX":               true,
 		"NAPTR":            true,
 		"NS":               true,
+		"OPENPGPKEY":       true,
 		"PTR":              true,
 		"SOA":              true,
 		"SRV":              true,
@@ -222,7 +223,7 @@ func checkTargets(rec *models.RecordConfig, domain string) (errs []error) {
 		}
 	case "SRV":
 		check(checkTarget(target))
-	case "CAA", "DHCID", "DS", "IMPORT_TRANSFORM", "SSHFP", "TLSA", "TXT":
+	case "CAA", "DHCID", "DS", "IMPORT_TRANSFORM", "SSHFP", "TLSA", "TXT", "OPENPGPKEY":
 	default:
 		if rec.Metadata["orig_custom_type"] != "" {
 			// it is a valid custom type. We perform no validation on target
@@ -283,6 +284,8 @@ func importTransform(srcDomain, dstDomain *models.DomainConfig, transforms []tra
 			// Not imported.
 			continue
 		case "LOC":
+			continue
+		case "OPENPGPKEY":
 			continue
 		default:
 			return fmt.Errorf("import_transform: Unimplemented record type %v (%v)",
@@ -692,6 +695,7 @@ var providerCapabilityChecks = []pairTypeCapability{
 	capabilityCheck("DHCID", providers.CanUseDHCID),
 	capabilityCheck("LOC", providers.CanUseLOC),
 	capabilityCheck("NAPTR", providers.CanUseNAPTR),
+	capabilityCheck("OPENPGPKEY", providers.CanUseOPENPGPKEY),
 	capabilityCheck("PTR", providers.CanUsePTR),
 	capabilityCheck("R53_ALIAS", providers.CanUseRoute53Alias),
 	capabilityCheck("SOA", providers.CanUseSOA),
